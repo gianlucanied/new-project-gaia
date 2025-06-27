d204f7cd6f5ffc432508fe30945584353c87cd09
Gianluca Nieddu <gianluca.nieddu96@gmail.com>
2025-06-27 08:42:16 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Home from './views/Home.vue'
+import About from './views/About.vue'
+import Header from './components/Header.vue'
+
+
+</script>
+
+<template>
+<Header></Header>
+  <router-view />
+</template>
+
+
+<style scoped>
+
+</style>
